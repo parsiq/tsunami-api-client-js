5a89005abba41b8d9e11f7f612ed4fbb337eeafc
TimurRassolov <timur@parsiq.net>
2023-02-10 12:52:33 +0200

    add auth and possible config

--- src/client/http-client.ts
+++ src/client/http-client.ts
@@ -1,11 +1,15 @@
 import axios, { AxiosInstance } from 'axios';
 
+export type TsunamiApiClientConfig = {
+    retries: number;
+}
 export abstract class HttpClient {
     protected readonly instance: AxiosInstance;
 
-    public constructor(baseURL: string) {
+    public constructor(baseURL: string, apiKey: string, config?: TsunamiApiClientConfig) {
         this.instance = axios.create({
             baseURL,
+            auth: {username: apiKey, password: ''}
         });
     }
 }
