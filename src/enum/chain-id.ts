49348a1565d88243266685e0d5de38ecd7c3cdee
Alexey Rehov <alexey.rehov@gmail.com>
2023-06-20 10:20:36 +0300

    add Metis chain id

--- src/enum/chain-id.ts
+++ src/enum/chain-id.ts
@@ -6,6 +6,7 @@ export enum ChainId {
   BNB_MAINNET = 'eip155-56', // BNB Chain Mainnet
   POLYGON_MAINNET = 'eip155-137', // Polygon Mainnet
   ARBITRUM_NITRO_MAINNET = 'eip155-42161', // Arbitrum Mainnet
+  METIS_MAINNET = 'eip155-1088', // Metis Mainnet
   BNB_TESTNET = 'eip155-97',
   POLYGON_MUMBAI = 'eip155-80001',
 }
