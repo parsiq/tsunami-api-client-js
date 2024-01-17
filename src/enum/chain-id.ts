8bf610541f2435219d964b5fe9ae8ec1e8227306
Alexey Rehov <alexey.rehov@gmail.com>
2024-01-17 14:22:10 +0200

    add Arbitrum Sepolia chain ID

--- src/enum/chain-id.ts
+++ src/enum/chain-id.ts
@@ -6,6 +6,7 @@ export enum ChainId {
   BNB_MAINNET = 'eip155-56', // BNB Chain Mainnet
   POLYGON_MAINNET = 'eip155-137', // Polygon Mainnet
   ARBITRUM_NITRO_MAINNET = 'eip155-42161', // Arbitrum Mainnet
+  ARBITRUM_NITRO_SEPOLIA = 'eip155-421614', // Arbitrum Sepolia
   METIS_MAINNET = 'eip155-1088', // Metis Mainnet
   POLYGON_ZKEVM_MAINNET = 'eip155-1101', // Polygon zkEVM Mainnet
   BNB_TESTNET = 'eip155-97',
